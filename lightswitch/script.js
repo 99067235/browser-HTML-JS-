7be893ede6e4596e94ac3c08a30383a2ee57af23
unknown <99067235@mydavinci.nl>
2022-02-22 12:54:10 +0100

    lightswitch html gemaakt

--- lightswitch/script.js
+++ lightswitch/script.js
@@ -1,10 +1,23 @@
 var btn = document.createElement('button');
 btn.style.margin = '10px';
-btn.innerHTML = '...';
+btn.innerHTML = 'turn light on';
 document.body.appendChild(btn);
+document.body.style.background = 'black';
+
 
 // schijf hier tussen je code
-
-
-
+var lightswitch = 'off'
+btn.addEventListener("click", ()=>{
+    if(lightswitch === 'off'){
+        btn.innerHTML = "turn light off"
+        lightswitch = 'on';
+        document.body.style.background = 'yellow';
+        console.log("light is now on")
+    }else{
+        btn.innerHTML = 'turn light on';
+        lightswitch = 'off';
+        document.body.style.background = 'black';
+        console.log("light is now off")
+    }
+})
 // schijf hier tussen je code
